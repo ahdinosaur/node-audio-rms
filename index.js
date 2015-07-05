1d1304ef3eee51938afa2677034d0ee9732860ef
Michael Williams <dinosaur@riseup.net>
2015-07-05 15:33:49 +1200

    improve rms

--- index.js
+++ index.js
@@ -6,55 +6,57 @@ module.exports = audioRms
 
 function audioRms () {
   // supports rms over multiple channels
-  
-  var root = cwise({
+
+  var rootOp = cwise({
     args: ['array'],
     body: function (x) {
       x = Math.sqrt(x)
     }
   })
 
-  /*
-  var mean = cwise({
-    args: ['shape', 'index', 'array'],
-    pre: function (shape) {
-      this.length = shape[0]
-      this.sum = zeros([this.length])
-    },
-    body: function (shape, index, array) {
-      this.sum.set(index[0], array.get(index[0], index[i]))
-    },
-    post: function () {
-      return this.sum / this.length
-    }
-  })
-  */
-
-  var mean = function mean (array) {
-    var length = array.shape[0]
-    var val = zeros([length])
-    for (var i = 0; i < length; i++) {
-      for (var j = 0; j < array.shape[1]; j++) {
-        val.set(i, val.get(i) + array.get(i, j))
-      }
-      val.set(i, val.get(i) / length)
-    }
-    return val
+  function root (audio) {
+    rootOp(audio)
+    return audio
   }
 
-  var square = cwise({
+  var sumOp = cwise({
+    args: ['array', 'array'],
+    body: function (sum, val) {
+      sum += val
+    }
+  })
+  
+  var divOp = cwise({
+    args: ['array', 'scalar'],
+    body: function (a, s) {
+      a /= s
+    }
+  })
+
+  function mean (audio) {
+    var meanShape = audio.shape.slice(0, audio.shape.length - 1)
+    var sum = zeros(meanShape, audio.dtype)
+
+    sumOp(sum, audio)
+    divOp(sum, audio.shape[audio.shape.length - 1])
+    
+    return sum
+  }
+
+  var squareOp = cwise({
     args: ['array'],
     body: function (x) {
       x = Math.pow(x, 2)
     }
   })
 
+  function square (audio) {
+    squareOp(audio)
+    return audio
+  }
+
   return through.obj(function (audio, enc, cb) {
-    square(audio)
-    audio = mean(audio)
-    root(audio)
-    cb(null, audio)
-    //cb(null, root(mean(square(audio))))
+    cb(null, root(mean(square(audio))))
   })
 }
 
