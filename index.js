1f8c0a87f3fee0ef83650a3d9b0e16a8137c0633
Michael Williams <dinosaur@riseup.net>
2015-07-05 19:00:32 +1200

    optimize ops, use existing cwise ops from ndarray-ops

--- index.js
+++ index.js
@@ -1,71 +1,34 @@
 var zeros = require('zeros')
 var through = require('through2')
-var cwise = require('cwise')
+var ops = require('ndarray-ops')
+var fill = require('ndarray-fill')
 
 module.exports = audioRms
 
 function audioRms () {
   // supports rms over multiple channels
 
-  var rootOp = cwise({
-    args: ['array'],
-    body: function (x) {
-      x = Math.sqrt(x)
-    }
-  })
-
   function root (audio) {
-    rootOp(audio)
+    ops.sqrteq(audio)
     return audio
   }
-
-  /*
-  var sumOp = cwise({
-    args: ['array', 'array'],
-    body: function (sum, val) {
-      sum += val
-    }
-  })
-  
-  var divOp = cwise({
-    args: ['array', 'scalar'],
-    body: function (a, s) {
-      a /= s
-    }
-  })
 
   function mean (audio) {
     var meanShape = audio.shape.slice(0, audio.shape.length - 1)
     var sum = zeros(meanShape, audio.dtype)
 
-    sumOp(sum, audio)
-    divOp(sum, audio.shape[audio.shape.length - 1])
+    fill(sum, function () {
+      arguments[arguments.length] = null
+      return ops.sum(audio.pick.apply(audio, arguments))
+    })
+
+    ops.divseq(sum, audio.shape[audio.shape.length - 1])
     
     return sum
   }
-  */
-
-  var mean = function mean (array) {
-    var length = array.shape[1]
-    var val = zeros([length])
-    for (var i = 0; i < length; i++) {
-      for (var j = 0; j < array.shape[1]; j++) {
-        val.set(i, val.get(i) + array.get(i, j))
-      }
-      val.set(i, val.get(i) / length)
-    }
-    return val
-  }
-
-  var squareOp = cwise({
-    args: ['array'],
-    body: function (x) {
-      x = Math.pow(x, 2)
-    }
-  })
 
   function square (audio) {
-    squareOp(audio)
+    ops.powseq(audio, 2)
     return audio
   }
 
@@ -73,5 +36,3 @@ function audioRms () {
     cb(null, root(mean(square(audio))))
   })
 }
-
-function mult (x, y) { return x * y }
